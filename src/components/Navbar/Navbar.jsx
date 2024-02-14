2c6762dcebe82bce3cd8d67c02db8a9c14aa8208
Anuj Sharma <anuj992393@gmail.com>
2024-02-14 09:15:56 +0530

    Added husky config

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import './Navbar.module.scss';
-import logo from '../../../public/res/logo.png';
 
 export default function Navbar() {
   return <nav className="flex flex-auto flex-row justify-between">NavBar</nav>;
