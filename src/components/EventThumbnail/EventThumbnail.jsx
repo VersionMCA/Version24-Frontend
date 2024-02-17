5df51769e1e330c81a3d96b5748fb25366447ebc
ChakshuVerma <vermachakshu@gmail.com>
2024-02-17 23:49:05 +0530

    Fixed merge issue

--- src/components/EventThumbnail/EventThumbnail.jsx
+++ src/components/EventThumbnail/EventThumbnail.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import './EventThumbnail.scss';
 
 function EventThumbnail({ id, imgLink, name, setNewItemActive }) {
+  const thumbnailClass = `thumbnail__item ${id === 0 ? 'active' : ''}`;
   return (
     <div
-      className="thumbnail__item"
+      className={thumbnailClass}
       onClick={() => setNewItemActive(id)}
       aria-hidden="true"
     >
