c119a98374e88be87b5e5805f4ead7e6ef5028e7
ChakshuVerma <vermachakshu@gmail.com>
2024-02-19 21:08:34 +0530

    Added Tertiary btn

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './EventCard.scss';
+import Button from '../Button/Button';
 
 function EventCard({ name, date, content, imgLink }) {
   return (
@@ -9,6 +10,12 @@ function EventCard({ name, date, content, imgLink }) {
         <p className="font-primary">{date}</p>
         <h2 className="font-primary">{name}</h2>
         <p>{content}</p>
+        <div className="btn-container">
+          <Button designType="tertiary" className="btn-register">
+            <span>Register</span>
+            <i />
+          </Button>
+        </div>
       </div>
     </div>
   );
--- src/pages/EventsPage/EventList.js
+++ src/pages/EventsPage/EventList.js
@@ -1,75 +1,99 @@
 const eventList = [
   {
     id: 1,
-    name: 'CodeBlitz - 1',
+    name: 'CodeBlitz',
     date: '21st March 2024',
     imgLink: `./src/assets/ImmerseCraft.jpg`,
-    // content: 'This is the first event of CodeBlitz',
     content:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora, fugiat repellendus, sit dignissimos distinctio repudiandae, ipsa at architecto suscipit culpa libero cumque aliquam quasi ipsum a? Numquam voluptas ullam enim!Officiis facilis itaque hic, consequatur facere expedita quisquam, harum maiores optio quidem architecto dolorum voluptatum sapiente reiciendis delectus, esse cupiditate repellat sunt doloribus debitis in aspernatur. Doloremque inventore quo eum!',
+      'CodeBlitz is an online coding competition for all the coders out there. It is a 2-hour long competition where you can solve problems and win exciting prizes.',
   },
   {
     id: 2,
-    name: 'CodeBlitz - 2',
+    name: 'ImmerseCraft',
     date: '21st March 2024',
     imgLink: `./src/assets/ImmerseCraft.jpg`,
-    content: 'This is the second event of CodeBlitz',
+    content:
+      ' Join the 1-hour guest lecture by the chief guest of Version 24, delving into Generative AI. Explore its advancements, challenges, and ethical dimensions. Discover practical applications and industry use cases,    gaining insights into its impact across fields. Engage in a Q&A session    for a deeper understanding of this transformative technology.',
   },
   {
     id: 3,
-    name: 'CodeBlitz - 3',
+    name: ' Cryptic Coder',
     date: '21st March 2024',
     imgLink: `./src/assets/img1.jpg`,
-    content: 'This is the third event of CodeBlitz',
+    content:
+      'CrypticCoder offers a challenging competition where participants must unravel and debug jumbled code within a 1-hour timeframe. The contest comprises four intricate questions, assessing coding aptitude and debugging skills. Success hinges on identifying and fixing errors, ensuring the corrected code passes hidden test cases',
   },
   {
     id: 4,
-    name: 'CodeBlitz - 4',
+    name: 'ImmerseCraft',
     date: '21st March 2024',
     imgLink: `./src/assets/img1.jpg`,
-    content: 'This is the fourth event of CodeBlitz',
+    content:
+      ' Generative Image Marvels invites participants to explore creativity through Generative AI tools. Given a theme, participants unleash imagination, creating captivating visuals with no tool or image limits. The event celebrates limitless possibilities, encouraging innovative and thought-provoking pieces. Join us in this journey of exploration and innovation in generative image generation',
   },
   {
     id: 5,
-    name: 'CodeBlitz - 5',
+    name: 'DecodeCraft',
     date: '21st March 2024',
     imgLink: `./src/assets/img1.jpg`,
-    content: 'This is the fifth event of CodeBlitz',
+    content:
+      ' Join the 1-hour fun-coding event where participants decode input/output examples to unveil problem statements. Translate deciphered problems into functional code against the clock. Top coders win recognition and prizes, adding excitement to the challenge',
   },
   {
     id: 6,
-    name: 'CodeBlitz - 6',
+    name: 'GameFiesta',
     date: '21st March 2024',
     imgLink: `./src/assets/img2.jpg`,
-    content: 'This is the sixth event of CodeBlitz',
+    content:
+      'The gaming event of Version’24. The game Zombie Apocalypse is a third person shooter PC game developed by our second year MCA student, Rishabh Jain. In order to win, you must achieve a score higher than the other players on the leaderboard, finishing the objectives in the least amount of time',
   },
   {
     id: 7,
-    name: 'CodeBlitz - 7',
+    name: ' Squid Quiz',
     date: '21st March 2024',
     imgLink: `./src/assets/img2.jpg`,
-    content: 'This is the seventh event of CodeBlitz',
+    content:
+      ' Squid Quiz, inspired by Squid Game, challenges participants with rapid-fire questions and strict time limits. A wrong answer leads to elimination, mimicking the high-stakes nature of Squid Game. The top ten advance to a tougher second round in this one-hour quiz on the Version website, providing an intense and competitive experience',
   },
   {
     id: 8,
-    name: 'CodeBlitz - 8',
+    name: ' DevDay',
     date: '21st March 2024',
     imgLink: `./src/assets/img2.jpg`,
-    content: 'This is the eighth event of CodeBlitz',
+    content:
+      "It is a team event where participants will be given some problem statements (basically some dev work) which they have to build. It won't be really difficult and won't be really easy as well. This will consist of some basic features, then some extra features can be added which will give some brownie points. 3-4 such questions can be given and they have to get as many points as possible. Team size : at max 2 members",
   },
   {
     id: 9,
-    name: 'CodeBlitz - 9',
+    name: 'Heads Up',
     date: '21st March 2024',
     imgLink: `./src/assets/img2.jpg`,
-    content: 'This is the ninth event of CodeBlitz',
+    content:
+      'Teams of 4-5 players, one with a phone near their forehead, try to guess words their teammates convey without speaking. Non-verbal clues determine success, with the team conveying the most words winning the challenge.',
   },
   {
     id: 10,
-    name: 'CodeBlitz - 10',
+    name: 'Cupocalyse Combat',
     date: '21st March 2024',
     imgLink: `./src/assets/img2.jpg`,
-    content: 'This is the tenth event of CodeBlitz',
+    content:
+      " It is a 3-member team event where each team member will serve one of the below three roles: 1. Constructor: Builds the castle using cups for defense. 2. Bombardier: Throws balls at opponent's castles to destroy them. 3. Supply Maven: Collects and supplies balls to the Bombardier for strategic attacks. Teams battle to defend and attack castles using provided balls. The team with the most intact castle levels at the end wins",
+  },
+  {
+    id: 11,
+    name: 'Map Quest',
+    date: '21st March 2024',
+    imgLink: `./src/assets/img2.jpg`,
+    content:
+      '  An unique digital treasure hunting experience 1. Treasure hunters embark on three sequential routes, each displayed       on a map. 2. Each route contains multiple pointers with clues indicating vital information about associated cities and their answers. 3. Routes require specific types of answers for progression. 4. Additional clues can be obtained by expending points. 5. Upon completing each route, participants must answer a corresponding question',
+  },
+  {
+    id: 12,
+    name: 'Destinite',
+    date: '21st March 2024',
+    imgLink: `./src/assets/img2.jpg`,
+    content:
+      "An exclusive interview round for Version'24 top performers. The top 3 participants, selected based on their exceptional performance throughout the events, will undergo a mock PI after submitting their resumes. The coveted 1st prize awaits the standout participant, promising ultimate recognition and reward for excellence at Destinite",
   },
 ];
 
